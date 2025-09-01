62b20d8cae5e38cb32c75eec346dae2b1b3853a6
Rida Malik <internalprogression@gmail.com>
2025-09-01 10:43:13 +0500

    Added code for main file and art file

---
+++ main.py
@@ -0,0 +1,43 @@
+from art import logo
+print(logo)
+
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+def calculator():
+    first_value = float(input("What is the first value?: \n"))
+    continue_calculation= True
+
+    while continue_calculation:
+        method = input("Pick an operation: +, -, *, /: \n")
+        second_value = float(input("What is your second value?: \n"))
+
+        calc_function = operations[method]
+        result = calc_function(first_value, second_value)
+        print(f"{first_value} {method} {second_value} = {result}")
+
+        should_continue = input("Type 'yes' to continue with the current result, or 'no' to start fresh:\n")
+        if should_continue == "yes":
+            first_value = result #carry forward the result
+        else:
+            continue_calculation = False
+            print("\n" * 20)
+            calculator()
+
+calculator()
